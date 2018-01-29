a4b7ba43d1988c29230bfc4396211965bff89db5
JDC0920 <942927387@qq.com>
2018-01-29 15:12:56 +0800

    首页轮播图完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,97 @@
 <template>
     <div>
-        <b-container>
-            <b-row>
-                <b-col cols="9">我占九个</b-col>
-                <b-col cols="3">我占三个</b-col>
-            </b-row>
-        </b-container>
+        <my-header></my-header>
+        <div class="index my-container">
+            <div class="row">
+                <div class="col-8 main">
+                    <!--banner-->
+                        <div v-swiper:mySwiper="swiperOption" class="banner">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide" v-for="banner in banners">
+                                    <img :src="banner">
+                                </div>
+                            </div>
+                            <div class="swiper-pagination swiper-pagination-white"></div>
+                            <div class="swiper-button-next swiper-button-white"></div>
+                            <div class="swiper-button-prev swiper-button-white"></div>
+                        </div>
+                </div>
+                <div class="col-4 aside">
+                    <div class="board">
+                        <nuxt-link to="/">
+                            <img src="../assets/img/1.png" alt="">
+                        </nuxt-link>
+                        <nuxt-link to="/">
+                            <img src="../assets/img/2.png" alt="">
+                        </nuxt-link>
+                        <nuxt-link to="/">
+                            <img src="../assets/img/3.png" alt="">
+                        </nuxt-link>
+                        <nuxt-link to="/">
+                            <img src="../assets/img/4.png" alt="">
+                        </nuxt-link>
+                        <nuxt-link to="/">
+                            <img src="../assets/img/5.png" alt="">
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+    import myHeader from '~/components/myHeader'
     export default {
-
+        data(){
+            return {
+                banners:[
+                    '/lunbo1.jpg',
+                    '/lunbo2.jpg',
+                    '/lunbo3.jpg',
+                    '/lunbo4.jpg'
+                ],
+                swiperOption: {
+                    loop: true,
+                    slidesPerView: 'auto',
+                    centeredSlides: true,
+                    spaceBetween: 30,
+                    autoplay:{
+                        delay:3000,//切换时间
+                        stopOnLastSlide:false,//是否停止在最后一张
+                        disableOnInteraction:false //禁止轮播
+                    },
+                    pagination: {
+                        el: '.swiper-pagination'
+                    },
+                    navigation:{
+                        nextEl:'.swiper-button-next',
+                        prevEl:'.swiper-button-prev'
+                    },
+                    on: {
+                        slideChange() {
+                            console.log('onSlideChangeEnd', this);
+                        },
+                        tap() {
+                            console.log('onTap', this);
+                        }
+                    }
+                }
+            }
+        },
+        components:{
+            myHeader
+        },
+        mounted() {
+            console.log('app init', this)
+            setTimeout(() => {
+                this.banners.push('/lunbo5.jpg')
+//                console.log('banners update')
+            }, 3000)
+            console.log(
+                'This is current swiper instance object', this.mySwiper,
+                'I will slideTo banners 5')
+            this.mySwiper.slideTo(3)
+        }
     }
 </script>
 <style>
